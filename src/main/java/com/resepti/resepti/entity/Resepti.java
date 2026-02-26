5a93f3cbc4abed80a398908576b9a6d68cc00b93
Atte Ampuja <atteampuja1@gmail.com>
2026-02-26 12:13:45 +0200

    Tag entity luotu ja ManyToMany relaatio lisätty

--- src/main/java/com/resepti/resepti/entity/Resepti.java
+++ src/main/java/com/resepti/resepti/entity/Resepti.java
@@ -9,6 +9,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
@@ -40,6 +43,13 @@ public class Resepti {
 
   @OneToMany(mappedBy = "resepti", cascade = CascadeType.ALL)
   private List<ReseptiAines> ainekset = new ArrayList<>();
+
+  @ManyToMany
+  @JoinTable(name = "resepti_tag",
+    joinColumns = @JoinColumn(name = "resepti_id"),
+    inverseJoinColumns = @JoinColumn(name = "tag_id")
+  )
+  private List<Tag> tags = new ArrayList<>();
 
   public Resepti() {}
 
